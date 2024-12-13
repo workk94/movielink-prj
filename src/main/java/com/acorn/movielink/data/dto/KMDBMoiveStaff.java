f6936cd91ad1dd31e6fce8d64914b89e0f37ab98
kdh <thy09177@gmail.com>
2024-12-13 17:49:12 +0900

    1213

--- src/main/java/com/acorn/movielink/data/dto/KMDBMoiveStaff.java
+++ src/main/java/com/acorn/movielink/data/dto/KMDBMoiveStaff.java
@@ -1,4 +1,4 @@
-package com.acorn.movielink.data;
+package com.acorn.movielink.data.dto;
 
 
 import lombok.AllArgsConstructor;
@@ -10,5 +10,6 @@ public class KMDBMoiveStaff {
 
     String staffNm;
     String staffEnNm;
+    String staffId;
 
 }
