a3c1d08da36d14f59373e2f6115e2f26f82e068c
K_Ho <ysking463@gmail.com>
2024-12-27 17:50:38 +0900

    feat/랭킹 페이지 구현

--- src/main/java/com/acorn/movielink/ranking/service/RankingService.java
+++ src/main/java/com/acorn/movielink/ranking/service/RankingService.java
@@ -1,4 +1,21 @@
 package com.acorn.movielink.ranking.service;
 
-public interface RankingService {
+import com.acorn.movielink.ranking.dto.RankingDTO;
+import com.acorn.movielink.ranking.repository.RankingMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RankingService {
+
+    private final RankingMapper rankingmapper;
+
+    public RankingService(RankingMapper rankingmapper) {
+        this.rankingmapper = rankingmapper;
+    }
+
+    public List<RankingDTO> selectMovieByRank() {
+        return rankingmapper.selectMovieByRank();
+    }
 }
