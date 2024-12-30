181fe4b30d4fda917f8c72897dfbd6c6eb792a7b
workk94 <workk219@gmail.com>
2024-12-30 18:09:26 +0900

    검색 뷰 페이지 수정, 랭킹 수정

--- src/main/java/com/acorn/movielink/search/controller/SearchController.java
+++ src/main/java/com/acorn/movielink/search/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.acorn.movielink.search.controller;
 
+import com.acorn.movielink.data.dto.MovieDTO;
+import com.acorn.movielink.login.service.MovieService;
 import com.acorn.movielink.search.dto.SearchMovieDTO;
 import com.acorn.movielink.search.dto.SearchPeopleDTO;
 import com.acorn.movielink.search.dto.SearchPostDTO;
@@ -7,21 +9,37 @@ import com.acorn.movielink.search.service.SearchServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
-    @Autowired
+
     private SearchServiceImpl searchService;
+    private MovieService movieService;
+
+
+    @Autowired
+    public SearchController(SearchServiceImpl searchService, MovieService movieService) {
+        this.searchService = searchService;
+        this.movieService = movieService;
+    }
 
     @GetMapping
-    public String searchPage() {
+    public String searchPage(Model model) {
+
+        // 추천영화 10개 반환
+        List<MovieDTO> recommendedMovies = movieService.getLatestMoviesByOpenDateDesc();
+        model.addAttribute("recommendedMovies", recommendedMovies);
+
         return "search";
     }
 
@@ -31,12 +49,7 @@ public class SearchController {
             @RequestParam(name = "offset", defaultValue = "0") int offset,
             @RequestParam(name = "limit", defaultValue = "4") int limit
     ) {
-        // 2글자 미만 검색어 처리
-        if (keyword.length() < 2) {
-            return ResponseEntity.badRequest().body(Map.of("error", "검색어는 최소 2글자 이상 입력해야 합니다."));
-        }
 
-        // 기존 로직 유지
         List<SearchMovieDTO> movies = searchService.searchMovie(keyword, offset, limit);
         List<SearchPeopleDTO> people = searchService.searchPeople(keyword, offset, limit);
         List<SearchPostDTO> posts = searchService.searchPosts(keyword, offset, limit);
