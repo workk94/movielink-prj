b49f9f547a026f6f1f65355090ca42876daf246c
workk94 <workk219@gmail.com>
2024-12-31 23:12:24 +0900

    게시글 이미지 업로드 기능 추가

--- src/main/java/com/acorn/movielink/config/WebConfig.java
+++ src/main/java/com/acorn/movielink/config/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${file.upload-dir}")
+    @Value("${file.base-dir}")
     private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
+        registry.addResourceHandler("/upload/**")
                 .addResourceLocations("file:///" + uploadDir);
     }
 }
